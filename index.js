a31adafad2940a9f7cd9987162d08e9baed1b4ba
Matthew McCormack <mmccormack1182@gmail.com>
2019-05-24 19:45:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+const logname = function (person){
+  console.log(person.name)
+}
+function logDriverNames(drivers) {
+  for (const driver of drivers) {
+    logname(driver)
+  }
+}
+
+function logDriversByHometown(drivers, location) {
+  for (const driver of drivers) {
+    if (driver.hometown === location) {
+      logname(driver)
+    }
+  }
+}
+
+function driversByRevenue(drivers) {
+  const driversCopy = [...drivers];
+  const revenueSorter = function(driverA, driverB) { 
+    return driverA.revenue - driverB.revenue
+  }
+  return driversCopy.sort(revenueSorter)
+}
+
+function driversByName(drivers) {
+  const driversCopy = [...drivers];
+  const nameSorter = function(driverA, driverB) { 
+    return driverA.name.localeCompare(driverB.name, 'en', {sensitivity: 'base'})
+  }
+  return driversCopy.sort(nameSorter)
+}
+
+
